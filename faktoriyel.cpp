42d9dda0ebbe2faf99b5e93041d3d1b6483ba847
layefhem <sametcelikbusiness@gmail.com>
2020-08-06 16:20:45 +0300

    faktoriyel eklendi

---
+++ faktoriyel.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void faktoriyelAl() {
+	int faktoriyel = 1;
+	for(int i=1 ; i <= 5 ; i++) {
+		faktoriyel = faktoriyel * i;
+	}
+	
+	cout << "faktoriyel= " << faktoriyel;
+}
+
+int main() {
+	faktoriyelAl();
+	}
